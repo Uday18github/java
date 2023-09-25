a77faaed73382ba7abdc4156df1b08c40fed08ee
Uday18github <137674905+Uday18github@users.noreply.github.com>
2023-09-25 11:20:55 +0530

    Create A2.java

---
+++ A2.java
@@ -0,0 +1,8 @@
+class A2{
+    static{
+        System.out.println("Inside static block");
+    }
+    public static void main(String args[]){
+        System.out.println("Inside the main method");
+    }
+}
