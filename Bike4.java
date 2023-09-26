a7f2e0dd0e7f980f7ce051b74354d9b95953da3f
Uday18github <137674905+Uday18github@users.noreply.github.com>
2023-09-26 09:33:46 +0530

    Add files via upload

---
+++ Bike4.java
@@ -0,0 +1,10 @@
+class Bike4{
+	final int speedlimit;
+	Bike4(){
+		speedlimit=40;
+		System.out.println("Speed limit is "+speedlimit);
+		}
+	public static void main(String args[]){
+		Bike4 b=new Bike4();
+		}
+	}
