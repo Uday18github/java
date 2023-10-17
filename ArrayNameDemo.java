2f24181e91330b8ed04ab84a2e4b092ad9a8c508
Uday18github <137674905+Uday18github@users.noreply.github.com>
2023-10-17 09:32:02 +0530

    Add files via upload

---
+++ ArrayNameDemo.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class ArrayNameDemo{
+	public static void main(String args[]){
+		int arr[]={11,22,33,44};
+		Class c=arr.getClass();
+		String name=c.getName();
+		System.out.println("The class name of the array is "+name);
+}}
